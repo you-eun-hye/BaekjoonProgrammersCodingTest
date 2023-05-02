3cf9390a56c3928932debb6874d84746169aa8a3
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-02 15:52:34 +0900

    2908번 상수

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        String[] arr = br.readLine().split(" ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        if(arr.length == 0){
-            System.out.print(0);
-            System.exit(0);
-        }
+        int reA = (A%10)*100 + ((A%100)/10)*10 + (A/100);
+        int reB = (B%10)*100 + ((B%100)/10)*10 + (B/100);
 
-        if(arr[0] == "") {
-            System.out.println(arr.length - 1);
-        }else{
-            System.out.println(arr.length);
-        }
+        System.out.print(reA>reB?reA:reB);
 
     }
 }
