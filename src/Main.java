f4f7511b754081f477192ff37f4aa7ee8dc27abd
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-05 17:36:41 +0900

    5622번 다이얼

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        String str = br.readLine();
+        int count = 0;
 
-        int reA = (A%10)*100 + ((A%100)/10)*10 + (A/100);
-        int reB = (B%10)*100 + ((B%100)/10)*10 + (B/100);
+        for(int i = 0; i < str.length(); i++){
+            switch (str.charAt(i)){
+                case 'A' : case 'B' : case 'C':
+                    count += 3;
+                    break;
 
-        System.out.print(reA>reB?reA:reB);
+                case 'D' : case 'E': case 'F' :
+                    count += 4;
+                    break;
 
+                case 'G' : case 'H': case 'I' :
+                    count += 5;
+                    break;
+
+                case 'J' : case 'K': case 'L' :
+                    count += 6;
+                    break;
+
+                case 'M' : case 'N': case 'O' :
+                    count += 7;
+                    break;
+
+                case 'P' : case 'Q': case 'R' : case 'S' :
+                    count += 8;
+                    break;
+
+                case 'T' : case 'U': case 'V' :
+                    count += 9;
+                    break;
+
+                case 'W' : case 'X': case 'Y' : case 'Z' :
+                    count += 10;
+                    break;
+            }
+        }
+        System.out.print(count);
     }
 }
