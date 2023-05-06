45d77faeec1a9d53fd315c6ba2fd7ba42fc34d99
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-06 16:56:12 +0900

    2444번 별 찍기 - 7

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int king = 1;
-        int queen = 1;
-        int rook = 2;
-        int bishop = 2;
-        int knight = 2;
-        int pawn = 8;
+        int num = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        king -= Integer.parseInt(st.nextToken());
-        queen -= Integer.parseInt(st.nextToken());
-        rook -= Integer.parseInt(st.nextToken());
-        bishop -= Integer.parseInt(st.nextToken());
-        knight -= Integer.parseInt(st.nextToken());
-        pawn -= Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= num; i++){
+            for(int j = 0; j < num-i; j++){
+                bw.write(' ');
+            }
+            for(int k = 0; k < i*2-1; k++){
+                bw.write('*');
+            }
+            bw.newLine();
+        }
 
-        System.out.print(king + " ");
-        System.out.print(queen + " ");
-        System.out.print(rook + " ");
-        System.out.print(bishop + " ");
-        System.out.print(knight + " ");
-        System.out.print(pawn + " ");
+        for(int i = num-1; i >= 0; i--){
+            for(int j = 0; j < num-i; j++){
+                bw.write(' ');
+            }
+            for(int k = 0; k < i*2-1; k++){
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
 
     }
 }
