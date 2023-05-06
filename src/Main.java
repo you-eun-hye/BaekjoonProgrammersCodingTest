00d527533bf7b6e16ba7b3761b25d83a625815df
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-06 16:26:45 +0900

    3003번 킹, 퀸, 룩, 비숍, 나이트, 폰

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int king = 1;
+        int queen = 1;
+        int rook = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        king -= Integer.parseInt(st.nextToken());
+        queen -= Integer.parseInt(st.nextToken());
+        rook -= Integer.parseInt(st.nextToken());
+        bishop -= Integer.parseInt(st.nextToken());
+        knight -= Integer.parseInt(st.nextToken());
+        pawn -= Integer.parseInt(st.nextToken());
+
+        System.out.print(king + " ");
+        System.out.print(queen + " ");
+        System.out.print(rook + " ");
+        System.out.print(bishop + " ");
+        System.out.print(knight + " ");
+        System.out.print(pawn + " ");
+
     }
 }
