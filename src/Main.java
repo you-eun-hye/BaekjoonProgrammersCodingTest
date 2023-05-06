c68a7824b2d7f98712cc767b13e395eec1ed11a8
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-06 16:25:26 +0900

    25083번 새싹

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str;
-        int count = 0;
-
-        while((str = br.readLine()) != null){
-            System.out.println(str);
-            count++;
-            if(count > 100){
-                break;
-            }
-        }
+    public static void main(String[] args){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
     }
 }
