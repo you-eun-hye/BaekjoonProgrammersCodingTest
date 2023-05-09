3ceed5bbe16b0e450d8b3c628ab2b1c96a7fe119
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-09 17:21:11 +0900

    1157번 단어 공부

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int answer = 1;
 
-        for(int i = 0; i < str.length()/2; i++){
-            if(str.charAt(i) != str.charAt(str.length()-i-1)){
-                answer = 0;
-                break;
+        int[] arr = new int[26]; // 알파벳 개수 총 26개
+        char st = '?';
+        int max = -1;
+        String str = br.readLine().toUpperCase(); // 대문자로 전환하여 받음
+
+        for(int i = 0; i < str.length(); i++){
+            // A는 인덱스 0이므로 해당하는 10진수인 65를 빼준다.
+            // 아래 코드처럼 A를 뺴주어도 된다.
+            arr[str.charAt(i)-'A']++;
+            if(max<arr[str.charAt(i)-'A']) {
+                max = arr[str.charAt(i) - 'A'];
+                st = str.charAt(i);
             }
+            else if(max==arr[str.charAt(i)-'A']) st='?';
         }
-        System.out.print(answer);
-        br.close();
-
+        System.out.print(st);
     }
 }
