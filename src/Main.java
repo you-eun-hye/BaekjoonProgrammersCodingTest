648895ef10ae3f32dc189a866dd2d2a8bbbe36b8
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-14 15:12:40 +0900

    10798번 세로읽기

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int[][] arr = new int[9][9];
-        int max = 0;
-        int first = 0;
-        int second = 0;
+        char[][] arr = new char[5][15];
+        String str = "";
 
-        for(int i = 0; i < 9; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 9; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] > max){
-                    max = arr[i][j];
-                    first = i+1;
-                    second = j+1;
-                }
-                else if(max == 0){
-                    first = 1;
-                    second = 1;
-                }
+        for(int i = 0; i < arr.length; i++){
+            str = br.readLine();
+            for(int j = 0; j < str.length(); j++){
+                arr[i][j] = str.charAt(j);
             }
         }
-        System.out.println(max);
-        System.out.println(first + " " + second);
 
+        for(int j = 0; j < 15; j++){
+            for(int i = 0; i < 5; i++){
+                if(arr[i][j] == '\0') continue;
+                System.out.print(arr[i][j]);
+            }
+        }
         br.close();
     }
 }
