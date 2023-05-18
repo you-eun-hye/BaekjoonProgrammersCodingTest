f3dc6a4c28b29fc938cff71a0988847471e5f34c
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-18 15:21:20 +0900

    2903번 중앙 이동 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int Quarter = 25;
-        int Dime = 10;
-        int Nickel = 5;
-        int Penny = 1;
-
-        int T = Integer.parseInt(br.readLine());
-        for(int i = 0; i < T; i++) {
-            int C = Integer.parseInt(br.readLine());
-            sb.append(C / Quarter + " ");
-            C %= Quarter;
-
-            sb.append(C / Dime + " ");
-            C %= Dime;
-
-            sb.append(C / Nickel + " ");
-            C %= Nickel;
-
-            sb.append(C / Penny + "\n");
-        }
-        System.out.print(sb);
-
+        int N = Integer.parseInt(br.readLine());
+        int dot = (int)Math.pow(2, N) + 1;
+        System.out.print(dot*dot);
         br.close();
     }
 }
