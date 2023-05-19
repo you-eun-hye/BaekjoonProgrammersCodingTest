c2738fae9253a6adef60a49015d65e05b1ea1de4
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-19 11:51:48 +0900

    1193번 분수 찾기

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,38 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int count = 1;
-        int range = 2;
 
-        if(N == 1){
-            System.out.print(1);
-        }
+        int X = Integer.parseInt(br.readLine());
+        int top = 1;
+        int bottom = 1;
+        int line = 1;
+        int n = 1;
+
+        if(X==1) System.out.print(top+"/"+bottom);
         else{
-            while(range <= N){
-                range = range + (6 * count);
-                count++;
+            while(n < X) {
+                line++;
+                n += line;
             }
-            System.out.print(count);
+            // 짝수행
+            if(line%2==0){
+                top = line;
+                for(int i = 0; i < n-X; i++){
+                    top--;
+                    bottom++;
+                }
+            }
+            // 홀수행
+            else{
+                bottom = line;
+                for(int i = 0; i < n-X; i++){
+                    top++;
+                    bottom--;
+                }
+            }
+            System.out.print(top+"/"+bottom);
         }
+
         br.close();
     }
 }
