0810f382c329802a5fc0c328d123636de23b83a5
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-20 15:31:02 +0900

    5086번 배수와 약수

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
 
-        String strA = st.nextToken();
-        String strB = st.nextToken();
-        int maxLength = Math.max(strA.length(), strB.length());
-        int[] A = new int[maxLength+1];
-        int[] B = new int[maxLength+1];
+        while(true){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-        int index = 0;
-        for(int i = strA.length()-1; i>=0;  i--, index++){
-            A[index] = strA.charAt(i) - '0';
+            if (x == 0 && y == 0) break;
+            else{
+                if(y%x == 0) System.out.println("factor");
+                else if(x%y == 0) System.out.println("multiple");
+                else System.out.println("neither");
+            }
         }
-
-        index = 0;
-        for(int i = strB.length()-1; i>=0;  i--, index++){
-            B[index] = strB.charAt(i) - '0';
-        }
-
-        for(int i = 0; i < maxLength; i++){
-            int value = A[i] + B[i];
-            A[i] = value % 10;
-            A[i+1] += value/10;
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        if(A[maxLength] != 0) sb.append(A[maxLength]);
-
-        for(int i = maxLength-1; i>=0; i--){
-            sb.append(A[i]);
-        }
-        System.out.print(sb);
 
         br.close();
     }
