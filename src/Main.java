f3d3c6555fde72dcfac36f298883b8a3d6619db0
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-20 14:40:05 +0900

    2869번 달팽이는 올라가고 싶다

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int X = Integer.parseInt(br.readLine());
-        int top = 1;
-        int bottom = 1;
-        int line = 1;
-        int n = 1;
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
 
-        if(X==1) System.out.print(top+"/"+bottom);
-        else{
-            while(n < X) {
-                line++;
-                n += line;
-            }
-            // 짝수행
-            if(line%2==0){
-                top = line;
-                for(int i = 0; i < n-X; i++){
-                    top--;
-                    bottom++;
-                }
-            }
-            // 홀수행
-            else{
-                bottom = line;
-                for(int i = 0; i < n-X; i++){
-                    top++;
-                    bottom--;
-                }
-            }
-            System.out.print(top+"/"+bottom);
-        }
+        int days = (V-A)/(A-B); // 도착 하루 전
+
+        // 정상에 올랐을 경우 미끄러지지 않음
+        if((V-A)%(A-B) != 0) days++;
+
+        System.out.print(days+1);
 
         br.close();
     }
