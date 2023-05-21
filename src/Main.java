a116a27c00eefe310ba9b0abf3543c08db0286e1
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-21 14:48:39 +0900

    1978번 소수 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true){
-            StringBuilder sb = new StringBuilder();
-            int n = Integer.parseInt(br.readLine());
-            if(n == -1) break;
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");;
+        int count = 0;
 
-            sb.append(n + " = 1");
-            int sum = 1;
-            for(int i = 2; i < n; i++){
-                if(n%i == 0){
-                    sb.append(" + " + i);
-                    sum+=i;
-                }
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            int check = 0;
+            for(int j = 1; j <=num; j++){
+                if(num%j == 0) check++;
             }
-
-            if(n == sum){
-                System.out.println(sb);
-            }
-            else{
-                System.out.println(n + " is NOT perfect.");
-            }
+            if(check == 2) count++;
         }
+        System.out.print(count);
+
         br.close();
     }
 }
