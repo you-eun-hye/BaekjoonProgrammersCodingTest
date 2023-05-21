dcf2e2a61d61914d56788bfecdb02cdab32ca491
[you-eun-hye] <[dkfvktorcp@naver.com]>
2023-05-21 14:37:20 +0900

    9506번 약수들의 합

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int count = 0;
-        int result = 0;
+        while(true){
+            StringBuilder sb = new StringBuilder();
+            int n = Integer.parseInt(br.readLine());
+            if(n == -1) break;
 
-        for(int i = 1; i <= N; i++){
-            if(N%i == 0) {
-                count++;
-                if(count == K){
-                    result = i;
-                    break;
+            sb.append(n + " = 1");
+            int sum = 1;
+            for(int i = 2; i < n; i++){
+                if(n%i == 0){
+                    sb.append(" + " + i);
+                    sum+=i;
                 }
             }
-        }
-        System.out.print(result);
 
+            if(n == sum){
+                System.out.println(sb);
+            }
+            else{
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
         br.close();
     }
 }
