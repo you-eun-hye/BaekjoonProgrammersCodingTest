768653c3393be4397a9aaee72d2c3666d232cc60
you-eun-hye <dkfvktorcp@naver.com>
2023-05-22 16:35:27 +0900

    27323번 직사각형

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int i = 2;
 
-        while(N != 1){
-            if(N%i == 0){
-                System.out.println(i);
-                N = N/i;
-            }
-            else i++;
-        }
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.print(A*B);
 
         br.close();
     }
