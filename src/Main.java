43943c9e031998cc9bb3073dbeacaf2f5bc1c1b8
you-eun-hye <dkfvktorcp@naver.com>
2023-05-22 16:45:23 +0900

    1085번 직사각형에서 탈출

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
-        System.out.print(A*B);
+        int min_x = Math.min(x, w-x);
+        int min_y = Math.min(y, h-y);
+
+        System.out.print(Math.min(min_x, min_y));
 
         br.close();
     }
