5dcacf2e73d11ffc8bd8b43908386f607b1bff14
you-eun-hye <dkfvktorcp@naver.com>
2023-05-25 17:06:49 +0900

    10101번 삼각형 외우기

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int minX = 10001;
-        int minY = 10001;
-        int maxX = -10001;
-        int maxY = -10001;
-        int N = Integer.parseInt(br.readLine());
+        int n1 = Integer.parseInt(br.readLine());
+        int n2 = Integer.parseInt(br.readLine());
+        int n3 = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            if(x < minX) minX = x;
-            if(x > maxX) maxX = x;
-
-            if(y < minY) minY = y;
-            if(y > maxY) maxY = y;
-        }
-        System.out.println((maxY-minY)*(maxX-minX));
+        if(n1 == n2 && n1 == n3 && n2 == n3 && n1 == 60)
+            System.out.println("Equilateral");
+        else if(n1+n2+n3 == 180 && (n1==n2 || n1==n3 || n2==n3) )
+            System.out.println("Isosceles");
+        else if(n1+n2+n3 == 180 && (n1!=n2 || n1!=n3 || n2!=n3) )
+            System.out.println("Scalene");
+        else System.out.println("Error");
 
         br.close();
     }
