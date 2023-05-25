3ff95b4442a0051310a44f38a6337a0202befdc8
you-eun-hye <dkfvktorcp@naver.com>
2023-05-25 17:01:03 +0900

    9063번 대지

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine());
-        System.out.println(4*n);
+        StringTokenizer st;
+
+        int minX = 10001;
+        int minY = 10001;
+        int maxX = -10001;
+        int maxY = -10001;
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if(x < minX) minX = x;
+            if(x > maxX) maxX = x;
+
+            if(y < minY) minY = y;
+            if(y > maxY) maxY = y;
+        }
+        System.out.println((maxY-minY)*(maxX-minX));
+
         br.close();
     }
 }
