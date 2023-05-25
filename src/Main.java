bb2ba15293938847ab2e7f62376c1554d796bc8d
you-eun-hye <dkfvktorcp@naver.com>
2023-05-25 17:20:07 +0900

    5073번 삼각형과 세 변

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n1 = Integer.parseInt(br.readLine());
-        int n2 = Integer.parseInt(br.readLine());
-        int n3 = Integer.parseInt(br.readLine());
+        while(true){
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            int n3 = Integer.parseInt(st.nextToken());
 
-        if(n1 == n2 && n1 == n3 && n2 == n3 && n1 == 60)
-            System.out.println("Equilateral");
-        else if(n1+n2+n3 == 180 && (n1==n2 || n1==n3 || n2==n3) )
-            System.out.println("Isosceles");
-        else if(n1+n2+n3 == 180 && (n1!=n2 || n1!=n3 || n2!=n3) )
-            System.out.println("Scalene");
-        else System.out.println("Error");
+            int maxN = 0;
+            if(n1>n2){
+                if(n2>n3) maxN = n1;
+                else maxN = (n1>n3) ? n1 : n3;
+            }
+            else maxN = (n2>n3) ? n2 : n3;
 
+            if(n1==n2 && n1==n3 && n2==n3 && n1==0) break;
+            if(maxN < n1+n2+n3-maxN){
+                if(n1==n2 && n1==n3 && n2==n3)
+                    System.out.println("Equilateral");
+                else if(n1==n2 || n1==n3 || n2==n3)
+                    System.out.println("Isosceles");
+                else if(n1!=n2 && n1!=n3 && n2!=n3)
+                    System.out.println("Scalene");
+            }
+            else System.out.println("Invalid");
+        }
         br.close();
     }
 }
