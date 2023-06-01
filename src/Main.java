11ee502a7115d749b897d7f8912b6712e191bb0e
you-eun-hye <dkfvktorcp@naver.com>
2023-06-01 13:35:19 +0900

    24313번 알고리즘 수업 - 점근적 표기 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long n = Long.parseLong(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
 
-        System.out.println((n*(n-1)*(n-2))/6);
-        System.out.println(3);
+        if((a1*n0 + a0 <= c*n0) && c>=a1) System.out.println(1);
+        else System.out.println(0);
 
         br.close();
     }
