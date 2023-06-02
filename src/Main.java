26852237f2102dc6c33874de7dcdc3f9521f40b9
you-eun-hye <dkfvktorcp@naver.com>
2023-06-02 20:03:51 +0900

    reupload

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int result = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[N];
 
         for(int i = 0; i < N; i++){
-            int num = i;
-            int sum = i;
+            arr[i] = br.readLine();
+        }
 
-            while(num != 0){
-                sum += num%10;
-                num/=10;
-            }
+        System.out.println(count(arr));
+        br.close();
+    }
 
-            if(sum == N){
-                result = i;
-                break;
+    public static int count(String[] arr){
+        int result = Integer.MAX_VALUE;
+
+        for(int i = 0; i < N-7; i++){
+            for(int j = 0; j < M-7; j++){
+                for(int k = 0; k < 2; k++){
+                    char start = k == 0 ? 'W' : 'B';
+
+                    int count = 0;
+                    for(int a = i; a < i+8; a++){
+                        for(int b = j; b < j+8; b++){
+                            if(start != arr[a].charAt(b)){
+                                count++;
+                            }
+                            start = start == 'W' ? 'B' : 'W';
+                        }
+                        start = start == 'W' ? 'B' : 'W';
+                    }
+                    result = Math.min(result, count);
+
+                    if(result == 0) return 0;
+                }
             }
         }
-        System.out.println(result);
-        br.close();
+        return  result;
     }
 }
