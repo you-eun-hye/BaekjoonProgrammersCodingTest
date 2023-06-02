06647df78fee5b3d90a3a3cc69feb3116ec5b0c2
you-eun-hye <[dkfvktorcp@naver.com]>
2023-06-02 22:39:26 +0900

    19532번 수학은 비대면강의입니다 ver1

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
-        String[] arr = new String[N];
+        int x = 0;
+        int y = 0;
 
-        for(int i = 0; i < N; i++){
-            arr[i] = br.readLine();
-        }
-
-        System.out.println(count(arr));
-        br.close();
-    }
-
-    public static int count(String[] arr){
-        int result = Integer.MAX_VALUE;
-
-        for(int i = 0; i < N-7; i++){
-            for(int j = 0; j < M-7; j++){
-                for(int k = 0; k < 2; k++){
-                    char start = k == 0 ? 'W' : 'B';
-
-                    int count = 0;
-                    for(int a = i; a < i+8; a++){
-                        for(int b = j; b < j+8; b++){
-                            if(start != arr[a].charAt(b)){
-                                count++;
-                            }
-                            start = start == 'W' ? 'B' : 'W';
-                        }
-                        start = start == 'W' ? 'B' : 'W';
-                    }
-                    result = Math.min(result, count);
-
-                    if(result == 0) return 0;
+        for(int i = -999; i < 1000; i++){
+            for(int j = -999; j < 1000; j++){
+                if((a*i+b*j==c) && (d*i+e*j==f)){
+                    x = i;
+                    y = j;
+                    break;
                 }
             }
         }
-        return  result;
+
+        System.out.println(x + " " + y);
+
+        br.close();
     }
 }
