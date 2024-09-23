4c1cfe0f7352e15247d0864ed54740188689f283
silver <88095254+you-eun-hye@users.noreply.github.com>
2024-09-23 13:10:26 +0900

    [Bronze V] Title: 검증수, Time: 164 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int temp = scanner.nextInt();
+            sum += temp * temp;
+        }
+
+        int result = sum % 10;
+        System.out.print(result);
+    }
+}
